e3248d9a1e303786fa8a7e05f7dce6799897f884
Daniel Marino <starzonmyarmz@gmail.com>
2011-12-12 20:49:28 -0500

    removed images because i made them data uris, bunch of other updates and tweaks. closer to nailing the height of the chat window and some better current class states on the app menu

--- reflow.user.js
+++ reflow.user.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
     $('#app_menu').append('<section id="my_flows"><h4>Flows</h4></section>')
     $('#app_menu').append('<section id="flow_filter"><h4>Filters</h4><ol></ol></section>');
 
-    var interval;
+    var interval, heightInterval;
 
     function moveTheDOM() {
 
@@ -48,14 +48,16 @@ $(document).ready(function(){
         // Move the filters list
         if (!filtersMoved && filtersList.length > 1) {
             filtersList.each(function () {
-                $(this).appendTo($('#flow_filter ol'))
+                $(this).appendTo($('#flow_filter ol'));
             });
             $('#flow_filter a')
                 .wrap('<li>')
                 .removeClass('app-button app-button-bevel ui-corner-left ui-state-default')
-                .find('span')
-                    .remove();
+                .find('span').remove();
             filtersMoved = true;
+            $('#flow_filter .ui-state-active')
+                .removeClass('ui-state-active')
+                .parent().addClass('active_item');
         }
 
         // Move the flow viewing options
@@ -63,18 +65,21 @@ $(document).ready(function(){
             viewsList.prepend('<section id="flow_views"><h4>Views</h4><ol></ol></section>');
             $('.app_icon').each(function() {
                 $(this).appendTo($('#flow_views ol'));
-                $(this).wrap('<li>')
+                $(this).wrap('<li>');
             });
             viewsMoved = true;
+            $('#flow_views .current')
+                .removeClass('current')
+                .parent().addClass('active_item');
         }
 
-        // Move the settings & help icon
+        // Move the settings, people & help icon
         if(!settingsMoved && actions.length === 1) {
             actions.attr('id', 'actions').appendTo($('#app_menu'));
             $('#actions a').removeClass('ui-corner-all ui-state-default app-button app-button-icon app-button-bevel app-button-toggleable app-button-icon-solo');
+            settingsMoved = true;
             $('#actions span').remove();
             $('#flowdock-influx .app-toolbar').remove();
-            settingsMoved = true;
         }
 
         // Move the users
@@ -106,22 +111,50 @@ $(document).ready(function(){
         }
 
         if (filtersMoved && flowListMoved && viewsMoved && settingsMoved && usersListMoved) {
+
             clearInterval(interval);
+
+            // Show selected menu items
+            $('#app_menu ol').children().click(function() {
+               $(this).closest('ol').children().removeClass('active_item');
+               $(this).closest('li').addClass('active_item')
+            });
+
         }
+
+        return false;
 
     }
 
-    interval = window.setInterval(moveTheDOM, 2000);
+    interval = window.setInterval(moveTheDOM, 3000);
 
     // Annoying but dealing with flow heights
     $(window).resize(function() {
-        var pSize    = $('#people').outerHeight() + $('header').height(),
-            contents = $('.app-contents'),
-            chat     = $('#chat_app_lines');
-        contents.height(contents.height() - pSize);
-        //chat.height(chat.height() - (pSize+70)); // Doesn't always fire correctly...
-        console.log(chat.html())
-        //chat.css("height", chat.height() + "px !important")
-    })
+        var top      = $('#people').outerHeight() + $('header').height(),
+            contents = $('.app-contents');
+        contents.height(contents.height() - top);
+        return false;
+    });
+
+    // Really annoying and seems really heavy. Checking to make sure all the
+    // elements exist before trying to resize the chat div. Also fires on window
+    // resize, which happens automatically every few seconds. Annoying!
+    $(window).resize(function() {
+        function setChatHeight() {
+            var chat     = $('#chat_app_lines'),
+                win      = $(window).height(),
+                people   = $('#people').outerHeight(),
+                header   = $('header').outerHeight(),
+                textarea = $('#chat_app_input_area').outerHeight();
+            if (chat.children().length > 0) {
+                if (header && people && textarea) {
+                    chat.height(win - (header + people + textarea))
+                    clearInterval(heightInterval);
+                }
+            }
+            return false;
+        }
+        heightInterval = window.setInterval(setChatHeight, 2000);
+    });
 
 });
